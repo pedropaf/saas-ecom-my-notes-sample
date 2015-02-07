2535c916d3d9916e9eae82d7f40dff27104e40de
pedropaf <pedro@pedroalonso.net>
2015-02-07 08:58:51 -0500

    Initial commit

---
+++ MyNotes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyNotes.MvcApplication" Language="C#" %>
---
+++ MyNotes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyNotes.Startup))]
+namespace MyNotes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
