800a507417c08704ac0d47dae74cdaa2b66d90e1
pedropaf <pedro@pedroalonso.net>
2015-02-09 10:11:12 -0500

    Added landing page to sample.

--- MyNotes/App_Start/BundleConfig.cs
+++ MyNotes/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace MyNotes
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
