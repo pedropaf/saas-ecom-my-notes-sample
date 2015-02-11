d0e6983ced3127898a46a7f87eb11e2459692cd8
pedropaf <pedro@pedroalonso.net>
2015-02-11 07:53:01 -0500

    Forgot / Reset password views updated.

--- MyNotes/Controllers/AccountController.cs
+++ MyNotes/Controllers/AccountController.cs
@@ -204,7 +204,7 @@ namespace MyNotes.Controllers
             if (ModelState.IsValid)
             {
                 var user = await UserManager.FindByNameAsync(model.Email);
-                if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                if (user == null)
                 {
                     // Don't reveal that the user does not exist or is not confirmed
                     return View("ForgotPasswordConfirmation");
