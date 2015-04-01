994da8c07d5889ae86a05edae655caf33adfdcd6
pedropaf <pedro@pedroalonso.net>
2015-04-01 08:03:59 -0500

    Notes Controller edited

--- MyNotes/Controllers/NotesController.cs
+++ MyNotes/Controllers/NotesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
@@ -7,18 +8,49 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using MyNotes.Models;
+using MyNotes.Views.SaasEcom.ViewModels;
+using SaasEcom.Core.DataServices.Storage;
+using SaasEcom.Core.Infrastructure.Facades;
+using SaasEcom.Core.Infrastructure.PaymentProcessor.Stripe;
 
 namespace MyNotes.Controllers
 {
     public class NotesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        private SubscriptionsFacade _subscriptionsFacade;
+        private SubscriptionsFacade SubscriptionsFacade
+        {
+            get
+            {
+                return _subscriptionsFacade ?? (_subscriptionsFacade = new SubscriptionsFacade(
+                    new SubscriptionDataService<ApplicationDbContext, ApplicationUser>
+                        (HttpContext.GetOwinContext().Get<ApplicationDbContext>()),
+                    new SubscriptionProvider(ConfigurationManager.AppSettings["StripeApiSecretKey"]),
+                    new CardProvider(ConfigurationManager.AppSettings["StripeApiSecretKey"],
+                        new CardDataService<ApplicationDbContext, ApplicationUser>(Request.GetOwinContext().Get<ApplicationDbContext>())),
+                    new CardDataService<ApplicationDbContext, ApplicationUser>(Request.GetOwinContext().Get<ApplicationDbContext>()),
+                    new CustomerProvider(ConfigurationManager.AppSettings["StripeApiSecretKey"]),
+                    new SubscriptionPlanDataService<ApplicationDbContext, ApplicationUser>(Request.GetOwinContext().Get<ApplicationDbContext>()),
+                    new ChargeProvider(ConfigurationManager.AppSettings["StripeApiSecretKey"])));
+            }
+        }
 
         // GET: Notes
         public async Task<ActionResult> Index()
         {
-            return View(await db.Notes.ToListAsync());
+            var userNotes =
+                await
+                    _db.Users.Where(u => u.Id == User.Identity.GetUserId())
+                        .Include(u => u.Notes)
+                        .Select(u => u.Notes)
+                        .ToListAsync();
+
+            return View(userNotes);
         }
 
         // GET: Notes/Details/5
@@ -28,7 +60,14 @@ namespace MyNotes.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Note note = await db.Notes.FindAsync(id);
+
+            ICollection<Note> userNotes = (await _db.Users.Where(u => u.Id == User.Identity.GetUserId()).Include(u => u.Notes).Select(u => u.Notes).FirstOrDefaultAsync());
+            if (userNotes == null)
+            {
+                return HttpNotFound();
+            }
+
+            Note note = userNotes.FirstOrDefault(n => n.Id == id);
             if (note == null)
             {
                 return HttpNotFound();
@@ -51,12 +90,34 @@ namespace MyNotes.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Notes.Add(note);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                if (await UserHasEnoughSpace(User.Identity.GetUserId()))
+                {
+                    note.CreatedAt = DateTime.UtcNow;
+                    _db.Notes.Add(note);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData.Add("flash", new FlashWarningViewModel("You can not add more notes, upgrade your subscription plan or delete some notes."));
+                }
             }
 
             return View(note);
+        }
+
+        private async Task<bool> UserHasEnoughSpace(string userId)
+        {
+            var subscription = (await SubscriptionsFacade.UserActiveSubscriptionsAsync(userId)).FirstOrDefault();
+
+            if (subscription == null)
+            {
+                return false;
+            }
+
+            var userNotes = await _db.Users.Where(u => u.Id == userId).Include(u => u.Notes).Select(u => u.Notes).CountAsync();
+
+            return subscription.SubscriptionPlan.GetPropertyInt("MaxNotes") > userNotes;
         }
 
         // GET: Notes/Edit/5
@@ -66,7 +127,12 @@ namespace MyNotes.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Note note = await db.Notes.FindAsync(id);
+            if (!await NoteBelongToUser(User.Identity.GetUserId(), noteId: id.Value))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            Note note = await _db.Notes.FindAsync(id);
             if (note == null)
             {
                 return HttpNotFound();
@@ -81,10 +147,10 @@ namespace MyNotes.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Text,CreatedAt")] Note note)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && await NoteBelongToUser(User.Identity.GetUserId(), note.Id))
             {
-                db.Entry(note).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                _db.Entry(note).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(note);
@@ -97,7 +163,12 @@ namespace MyNotes.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Note note = await db.Notes.FindAsync(id);
+            if (!await NoteBelongToUser(User.Identity.GetUserId(), noteId: id.Value))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Note note = await _db.Notes.FindAsync(id);
             if (note == null)
             {
                 return HttpNotFound();
@@ -110,17 +181,26 @@ namespace MyNotes.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Note note = await db.Notes.FindAsync(id);
-            db.Notes.Remove(note);
-            await db.SaveChangesAsync();
+            if (!await NoteBelongToUser(User.Identity.GetUserId(), noteId: id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Note note = await _db.Notes.FindAsync(id);
+            _db.Notes.Remove(note);
+            await _db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+        private async Task<bool> NoteBelongToUser(string userId, int noteId)
+        {
+            return await _db.Users.Where(u => u.Id == userId).Where(u => u.Notes.Any(n => n.Id == noteId)).AnyAsync();
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
+                _db.Dispose();
             }
             base.Dispose(disposing);
         }
